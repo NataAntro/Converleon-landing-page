ac054ea78887e0aa43cd5e39dc936ef2d721ccf6
Nataliya Antropova <86189611+NataAntro@users.noreply.github.com>
2025-11-23 11:07:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // ВАЖНО для GitHub Pages:
-  // имя репозитория с учетом регистра
-  base: "/Converleon-landing-page/",
-
+  base: "./",
   server: {
     host: "::",
     port: 8080,
